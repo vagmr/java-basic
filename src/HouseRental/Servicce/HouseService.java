6961fc15346b13728bf40ca84cbe204c864d43fb
vagmr <2737165206.qqcom>
2022-11-22 13:38:08 +0800

    update

--- src/HouseRental/Servicce/HouseService.java
+++ src/HouseRental/Servicce/HouseService.java
@@ -1,6 +1,8 @@
 package src.HouseRental.Servicce;
 
 import src.HouseRental.Model.HouseInformation;
+import src.HouseRental.View.HouseView;
+import src.HouseRental.utils.Utils;
 
 public class HouseService {
     private HouseInformation[] houses;//保存信息到对象数组中
@@ -11,7 +13,13 @@ public class HouseService {
     }
     public boolean add(HouseInformation hN){
         if(houseNums >= houses.length){
-            System.out.println("房屋信息已经不能添加");
+            System.out.println("你是否要扩容房屋信息(y/n)");
+            String opt = Utils.readString(1);
+            if( !housepluschoice(opt)) {
+                System.out.println("房屋信息已经不能添加");
+            } else if(housepluschoice(opt)){
+                return true;
+            }
             return false;
         }
         houses[houseNums++] = hN;
@@ -22,5 +30,23 @@ public class HouseService {
     //编写一个方法返回房屋的信息
     public HouseInformation[] list(){
         return houses;//house是一个对象数组
+    }
+    //编写一个方法扩容数组
+    public boolean housepluschoice(String opt){
+        if("y".equals(opt)){
+            System.out.println("房屋信息扩容成功");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public HouseInformation[] housesPlus(HouseInformation[] houses){
+        HouseService h2 = new HouseService(new HouseView().size + 1);
+        for (int i = 0; i <houses.length ; i++) {//给新数组赋值
+            h2.houses[i] = houses[i];
+        }
+        houses = h2.houses;
+        return h2.houses;
     }
 }
--- src/HouseRental/View/HouseView.java
+++ src/HouseRental/View/HouseView.java
@@ -5,17 +5,47 @@ import src.HouseRental.Model.HouseInformation;
 import src.HouseRental.Servicce.HouseService;
 import src.HouseRental.utils.Utils;
 
+
+
 public class HouseView {
 
 
     boolean opt = true;
     char choice;
-    private HouseService hv = new HouseService(10);
+    public int size = 2;
+    private HouseService hv = new HouseService(size);
+
+
+
+    public  void houseAdd(){
+        System.out.println("--------------新增房屋--------------");
+        System.out.print("姓名：");
+        String name = Utils.readString(5);
+        System.out.print("电话：");
+        String tel = Utils.readString(11);
+        System.out.print("地址：");
+        String address = Utils.readString(16);
+        System.out.print("月租：");
+        int rent = Utils.readInt();
+        System.out.print("状态：");
+        String state = Utils.readString(3);
+        HouseInformation Hn = new HouseInformation(0,name,tel,address,rent,state);
+        if(hv.add(Hn)){
+            System.out.println("新增房屋成功");
+        } else {
+            System.out.println("新增房屋失败");
+        }
+    }
     public  void houseList(){
         System.out.println("---------------房屋列表----------------");
         System.out.println("编号\t\t房主\t\t\t电话\t\t\t地址\t\t租金\t\t状态");
         HouseInformation[] Houses = hv.list();
+        Houses = hv.housesPlus(Houses);
         for (int i = 0; i <Houses.length ; i++) {
+            if(Houses[i] == null){
+                System.out.println("\n--------------房屋列表显示完毕-----------\n");
+                break;
+            }
             System.out.println(Houses[i]);
         }
     }
@@ -33,7 +63,8 @@ public class HouseView {
             choice = Utils.readChar();
             switch (choice) {
                 case '1':
-                    System.out.println("新 增");
+                    System.out.println("\t\t\t  新 增");
+                    houseAdd();
                     break;
                 case '2':
                     System.out.println("查 找");
@@ -45,7 +76,7 @@ public class HouseView {
                     System.out.println("修 改");
                     break;
                 case '5':
-                    System.out.println("列 表");
+                    houseList();
                     break;
                 case '6':
                     System.out.println("正在退出.........");
