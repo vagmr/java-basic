01cebe15470cc46f86f68e7008e1fa0b6f60a068
vagmr <2737165206.qqcom>
2023-02-11 13:44:47 +0800

    update

--- src/chapter06/Exception/t01.java
+++ src/chapter06/Exception/t01.java
@@ -9,23 +9,37 @@ import java.util.Scanner;
  * Chapter04
  */
 public class t01 {
+    // 主方法
     public static void main(String[] args) {
+        // 定义一个整数类型的变量
         int a;
+        // 定义一个字符串数组
         String[] b = new String[2];
+        // 创建 Scanner 对象
         Scanner myscanner = new Scanner(System.in);
+        // 打印提示信息
         System.out.println("请输入数字");
+        // 读取用户输入的整数
         a = myscanner.nextInt();
+        // 将代码放在 try-catch 块中
         try {
+            // 使用 do-while 循环
             do {
+                // 判断输入的数字是否不等于 1
                 if (a != 1) {
+                    // 如果不等于 1，打印“cu”并将 b[0] 设置为“cu”
                     System.out.println("cu");
                     b[0] = "cu";
                 } else {
+                    // 如果等于 1，打印“对”
                     System.out.println("对");
                 }
+                // 如果 b[1] 的值不等于“cu”，循环继续
             } while (!(b[1].equals("cu")));
         }
-        catch (NullPointerException e){
+        // 捕获 NullPointerException 异常
+        catch (NullPointerException e) {
+            // 如果捕获到异常，打印“异常已经捕捉”
             System.out.println("异常已经捕捉");
         }
     }
